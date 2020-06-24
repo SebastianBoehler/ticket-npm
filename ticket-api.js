523128e60c4b1ef877ac8fe5ca313441a9ab911d
Sebastian_B <basti.boehler@hotmail.de>
2020-06-25 01:41:56 +0200

    update

--- ticket-api.js
+++ ticket-api.js
@@ -68,9 +68,11 @@ async function rndString() {
 
 }
 
+
 module.exports = class TicketAPI {
     constructor(key) {
         this.key = key
+        this.sessions = {}
     };
 
     setIPAddress(ip) {
@@ -82,11 +84,43 @@ module.exports = class TicketAPI {
     };
 
     async startSession(proxy) {
+        //console.log('session Storage', this.sessions)
+        var headers = {}
         var isErrored = false
 
-        if (!this.IPAddress) return 'ip address required'
+        //console.log('received proxy', proxy)
+        this.proxy = proxy
 
-        const wasmbinsrc = await source()
+        if (!this.IPAddress) return 'ip address required'
+        var wasmbinsrc = null
+
+        var test = undefined
+        var expiration = new Date()
+        expiration.setMinutes(expiration.getMinutes() - 5)
+
+        for (var a in this.sessions) {
+            //console.log(a, proxy)
+            if (this.sessions[a]['time'] < expiration) {
+                delete this.sessions[a]
+                console.log('removing wasm file!')
+            }
+            else if (proxy === a) {
+                this.cookie = this.sessions[a]['cookie']
+                this.session = this.sessions[a]['session']
+                console.log('proxy found in database')
+                test = this.sessions[a]['session']
+                break
+            }
+        }
+
+        if (test) {
+            console.log('return only session data')
+            return {
+            session: test
+        }
+    }
+
+        wasmbinsrc = await source()
             .catch(e => {
                 console.log(e)
                 isErrored = true
@@ -94,7 +128,6 @@ module.exports = class TicketAPI {
 
         if (isErrored) return
 
-        var headers = {}
         if (this.cookie) headers['cookie'] = this.cookie
         //console.log('cookie used', params)
         //console.log('using proxy for _ticket checkout', this.proxy)
@@ -110,10 +143,15 @@ module.exports = class TicketAPI {
 
         if (proxy) params['agent'] = new HttpsProxyAgent(proxy)
 
+        //console.log(params)
         return new Promise(async (resolve, reject) => {
             await fetch(wasmbinsrc, params)
                 .then(async resp => {
-                    const path = __dirname + `/${await rndString()}.wasm`
+                    var path = __dirname + `/${await rndString()}.wasm`
+
+                    console.log('wasm endpoint status', resp.status)
+
+                    //console.log(JSON.stringify(this.sessions, null, 2))
                     console.log('requested wasm')
                     var file = fs.createWriteStream(path)
                     file.on("error", (function (e) {
@@ -141,23 +179,30 @@ module.exports = class TicketAPI {
                                 const cookie = resp.headers.get("set-cookie").split(';')[0]
                                 this.cookie = cookie
                             } catch (error) {
-        
+
                             }
 
                             resp = await resp.json()
 
                             if (resp['error'].includes('session not found in db')) this.cookie = undefined
-                            if (resp['success'])  {
+                            if (resp['success']) {
                                 this.session = resp['session']
+                                if (!proxy) proxy = 'localhost'
+                                this.sessions[proxy] = {
+                                    path: path,
+                                    session: this.session,
+                                    cookie: this.cookie,
+                                    time: new Date().getTime()
+                                }
+                                if (Number(resp['size']) < 40) console.log('wasm size seems invalid')
                                 resolve(resp)
-                            }
-                            else reject(resp)
+                            } else reject(resp)
                         })
                         .catch(err => {
                             reject(err)
                         })
 
-                    fs.unlinkSync(path)
+                    //fs.unlinkSync(path)
                 }).catch(e => {
                     reject(e)
                 })
@@ -201,6 +246,7 @@ module.exports = class TicketAPI {
                     try {
                         const cookie = resp.headers.get("set-cookie").split(';')[0]
                         this.cookie = cookie
+                        this.sessions[this.proxy]['cookie'] = cookie
                     } catch (error) {
 
                     }
@@ -221,6 +267,6 @@ module.exports = class TicketAPI {
                 })
         })
     };
-}
+};
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
